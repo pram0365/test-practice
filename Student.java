87b658eb1b1a2c44f1eb65241ef2c266c1e9a80e
pram0365 <pram0365@gmail.com>
2024-06-24 20:46:53 +0530

    Update Student.java
    Second Commit

--- Student.java
+++ Student.java
@@ -2,4 +2,9 @@ class Student{
     public static void main(String args[]){
         System.out.println("HI") ;      
     }
+
+    public void getMethod(){
+         System.out.println("getMethod") ;     
+    }
+        
 }
